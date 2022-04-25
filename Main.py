6d78e677a777cfc0df145e80a0d126848bfd0969
Osama <osama_badawi@outlook.com>
2022-04-25 23:13:03 +0200

    final without cheeting

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
-from Operations import Matrix
+from Operations import Matrix, empty_matrix
 matrix_1=Matrix([[1,2],[3,4]])
 matrix_2=Matrix([[4,5,4],[6,7,1]])
 matrix_3=Matrix([[6,2],[2,5]])
 
-# print(matrix_1.add(matrix_3))
+matrix4=empty_matrix(2,2) #empty matrix ex
+print(matrix4)
 
-# print(matrix_1.subtract(matrix_1))
+print(matrix_1.add(matrix_3)) # addition ex
 
-print(matrix_1.multiply(matrix_2))
+print(matrix_1.subtract(matrix_1)) #subtraction ex
+
+print(matrix_1.multiply(matrix_2)) #multiplication ex
--- Operations.py
+++ Operations.py
@@ -1,4 +1,13 @@
-import numpy as np
+import copy
+def empty_matrix(m,n):
+    mat=[]
+    for i in range(m):
+        newmat=[]
+        for j in range(n):
+            newmat.append(0)
+        mat.append(newmat)
+    return mat
+
 class Matrix:
     def __init__(self,data:list[list[int]]):
         self.row=len(data)
@@ -6,19 +15,25 @@ class Matrix:
         self.data=data
 
     def add(self, other):
-        result=self.data.copy()
+        result=copy.deepcopy(self.data)
         for i in range(self.row) :
             for j in range(self.col) :
                 result[i][j] += other.data[i][j]
         return result
 
     def subtract(self,other):
-        result=self.data.copy()
+        result=copy.deepcopy(self.data)
         for i in range(self.row):
             for j in range(self.col):
                 result[i][j]-=other.data[i][j]
         return result
 
     def multiply(self,other):
-        result= np.dot(self.data,other.data)
-        return result
+        result=Matrix(empty_matrix(self.row,other.col))
+        for i in range(result.row):
+            for j in range(result.col):
+                for o in range(self.row):
+                    result.data[i][j]+=self.data[i][o] * other.data[o][j]
+        return result.data
+
+       
